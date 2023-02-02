67df724a233ee2d726c6a9f0ab0b9ecd4f51a2c6
jjeanjacques10 <jjean.jacques10@gmail.com>
2023-02-02 20:28:50 -0300

    consumer with different groups

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ Projects from the Udemy course [Apache Kafka](https://www.udemy.com/course/apach
 - [str-producer](./str-producer/)
 - [str-consumer](./str-consumer/)
 
-
 ### Getting Started
 
 ``` bash
@@ -16,6 +15,16 @@ docker-compose up -d
 
 - [Kafka Drop - http://localhost:19000/](http://localhost:19000/)
 - [Str Producer - http://localhost:8000/](http://localhost:8000/)
+
+## Notes
+
+- When it configs the same groupId for different @KafkaListener, they will listen to different partitions.
+
+``` bash
+group-1: partitions assigned: [str-topic-1]
+group-1: partitions assigned: [str-topic-0]
+group-2: partitions assigned: [str-topic-0, str-topic-1] # group-2 will listen to both partitions
+```
 
 ---
 Developed by [Jean Jacques Barros](https://github.com/jjeanjacques10/)
--- str-consumer/src/main/java/com/jjeanjacques/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/jjeanjacques/strconsumer/listeners/StrConsumerListener.java
@@ -9,8 +9,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
-    public void listener(String message) {
-        log.info("Receive message {}", message);
+    @KafkaListener(groupId = "group-0", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void create(String message) {
+        log.info("CREATE ::: Receive message {}", message);
     }
+
+    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void log(String message) {
+        log.info("LOG ::: Receive message {}", message);
+    }
+
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void history(String message) {
+        log.info("HISTORY ::: Receive message {}", message);
+    }
+
 }
