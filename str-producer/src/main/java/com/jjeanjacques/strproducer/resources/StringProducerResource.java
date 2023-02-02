d95aefe09cd1336f2940fdd43ec5967bb24693f3
jjeanjacques10 <jjean.jacques10@gmail.com>
2023-02-02 07:57:57 -0300

    create producer - send string method

---
+++ str-producer/src/main/java/com/jjeanjacques/strproducer/resources/StringProducerResource.java
@@ -0,0 +1,24 @@
+package com.jjeanjacques.strproducer.resources;
+
+import com.jjeanjacques.strproducer.services.StringProducerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/producer")
+@RequiredArgsConstructor
+public class StringProducerResource {
+
+    private final StringProducerService producerService;
+
+    @PostMapping
+    public ResponseEntity<?> sendMessage(@RequestBody String message) {
+        producerService.sendMessage(message);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
