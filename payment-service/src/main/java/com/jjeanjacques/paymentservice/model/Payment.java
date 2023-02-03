1a2c5580b63afff09ddca92ddcee6e8915b6a752
jjeanjacques10 <jjean.jacques10@gmail.com>
2023-02-03 08:48:07 -0300

    add producer config

--- payment-service/src/main/java/com/jjeanjacques/paymentservice/model/Payment.java
+++ payment-service/src/main/java/com/jjeanjacques/paymentservice/model/Payment.java
@@ -10,6 +10,6 @@ public class Payment implements Serializable {
     private Long id;
     private Long idUser;
     private Long idProduct;
-    private Long cardNumber;
+    private String cardNumber;
 
 }
--- payment-service/src/main/java/com/jjeanjacques/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/jjeanjacques/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,16 +2,29 @@ package com.jjeanjacques.paymentservice.service.impl;
 
 import com.jjeanjacques.paymentservice.model.Payment;
 import com.jjeanjacques.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
 
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
     @Override
+    @SneakyThrows
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento {}", payment);
+        log.info("Recebi o pagamento {}", payment);
+        Thread.sleep(1000);
+
+        log.info("Enviando pagamento...");
+        kafkaTemplate.send("payment-topic", payment);
     }
 
 }
